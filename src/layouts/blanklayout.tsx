497dab8e1a533e6c0d7aa4a4f940dcd139f511f8
quandv <quandv@vdcsoft.vn>
2024-08-29 22:14:45 +0700

    set up layout

---
+++ src/layouts/blanklayout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const BlankLayout = () => {
+  return (
+    <>
+      <h1>404</h1>
+    </>
+  );
+};
+
+export default BlankLayout;
---
+++ src/layouts/index.ts
@@ -0,0 +1,10 @@
+import BlankLayout from "./blanklayout"; 
+import LoginLayout from "./loginlayout";
+import MainLayout from "./mainlayout";
+
+
+export {
+    MainLayout,
+    BlankLayout,
+    LoginLayout
+}
